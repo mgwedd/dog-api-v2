573fe3f91247034814175bbf6d2e2fac1fe7c71a
mgwedd <michael.wedd@gmail.com>
2019-04-11 19:27:57 -0400

    refactored down to 22 lines

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
 'use strict';
 
 function getDogCount() {
-    console.log('getDogCount is running');
     $('input[type="submit"]').on('click', event => {
-        console.log('form submitted');
         event.preventDefault();
         getAndLogDogs($('input[type="text"]').val());
     });
 }
 
 function getAndLogDogs(dogCount) {
-    console.log('getAndLogDogs is running');
     fetch(`https://dog.ceo/api/breeds/image/random/${dogCount}`)
     .then(response => response.json())
     .then(responseObj => renderImagesInConsole(responseObj))
@@ -18,13 +15,8 @@ function getAndLogDogs(dogCount) {
 }
 
 function renderImagesInConsole(responseObj) {
-    console.log('renderImagesInConsole is running');
     const dogImagesArray = responseObj.message;
-    dogImagesArray.forEach(dogImage => {
-        // console.image('link') comes from this funny little libray: 
-        // https://ourcodeworld.com/articles/read/457/how-to-display-images-in-the-javascript-console-of-the-browser
-        console.image(dogImage);
-    });
+    dogImagesArray.forEach(dogImage => {console.image(dogImage)});
 }
 
 $(getDogCount());
